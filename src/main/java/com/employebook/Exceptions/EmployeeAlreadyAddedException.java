e67535ab0538c3b6443973cef750f22766a583cc
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-04-15 04:22:55 +0300

    done

--- src/main/java/com/employebook/Exceptions/EmployeeAlreadyAddedException.java
+++
@@ -1,7 +0,0 @@
-package com.employebook.Exceptions;
-
-public class EmployeeAlreadyAddedException extends RuntimeException {
-    public EmployeeAlreadyAddedException() {
-        super("Сотрудник уже добавлен");
-    }
-}
---
+++ src/main/java/com/employebook/controller/EmployeeBookController.java
@@ -0,0 +1,32 @@
+package com.employebook.controller;
+
+import com.employebook.Employee;
+import com.employebook.service.EmployeeService;
+import com.employebook.service.EmployeeServiceImpl;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/departments")
+public class EmployeeBookController {
+    private final EmployeeService employeeService = new EmployeeServiceImpl();
+
+    @GetMapping("/max-salary")
+    public Employee findMaxSalary(@RequestParam("department") int department) {
+        return employeeService.findMaxSalary(department);
+    }
+    @GetMapping("/min-salary")
+    public Employee findMinSalary(@RequestParam("department") int department) {
+        return employeeService.findMinSalary(department);
+    }
+    @GetMapping("/department-employees")
+    public List<Employee> printAllDepartment(@RequestParam("department") int department) {
+        return employeeService.printAllDepartment(department);
+    }
+    @GetMapping("/all")
+    public List<Employee> printAll() {
+        return employeeService.printAll();
+    }
+
+}
