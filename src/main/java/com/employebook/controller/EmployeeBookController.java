9ae549a47d5277e5a810d55443872ebefe059246
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-04-15 04:44:53 +0300

    fin

--- src/main/java/com/employebook/controller/EmployeeBookController.java
+++ src/main/java/com/employebook/controller/EmployeeBookController.java
@@ -13,20 +13,21 @@ public class EmployeeBookController {
     private final EmployeeService employeeService = new EmployeeServiceImpl();
 
     @GetMapping("/max-salary")
-    public Employee findMaxSalary(@RequestParam("department") int department) {
-        return employeeService.findMaxSalary(department);
+    public String findMaxSalary(@RequestParam("department") int department) {
+        return employeeService.findMaxSalary(department).toString();
     }
     @GetMapping("/min-salary")
-    public Employee findMinSalary(@RequestParam("department") int department) {
-        return employeeService.findMinSalary(department);
+    public String findMinSalary(@RequestParam("department") int department) {
+        return employeeService.findMinSalary(department).toString();
     }
     @GetMapping("/department-employees")
-    public List<Employee> printAllDepartment(@RequestParam("department") int department) {
-        return employeeService.printAllDepartment(department);
+    public String printAllDepartment(@RequestParam("department") int department) {
+        return employeeService.printAllDepartment(department).toString();
     }
     @GetMapping("/all")
-    public List<Employee> printAll() {
-        return employeeService.printAll();
+    public String printAll() {
+        return employeeService.printAll().toString();
+
     }
 
 }
